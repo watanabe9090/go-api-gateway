85a04b89548e89ec3301de70633a77b6d721ef65
Yuri Takae Watanabe <yuriwatanabe9090@gmail.com>
2024-09-12 04:22:28 -0300

    :art: :white_check_mark: Improving returning statements and tests

--- internal/jwt-utils.go
+++ internal/jwt-utils.go
@@ -1,29 +1,30 @@
 package internal
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secretKey = []byte("your-secret-key")
-
 func CreateJwtToken(username string, role string, secret string) (string, error) {
+	var secretKey = []byte(secret)
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"sub": username,
 		"iss": "go-api-gateway",
 		"aud": role,
-		"exp": time.Now().Add(time.Hour).Unix(),
+		"exp": time.Now().Add(time.Hour).Unix(), // 1 Hour of expiration time
 		"iat": time.Now().Unix(),
 	})
 	tokenStr, err := claims.SignedString(secretKey)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 	return tokenStr, nil
 }
 
-func ValidJwtToken(tokenStr string) (jwt.Claims, error) {
+func ValidJwtToken(tokenStr string, secret string) (jwt.Claims, error) {
+	var secretKey = []byte(secret)
 	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 		return secretKey, nil
 	})
@@ -31,7 +32,7 @@ func ValidJwtToken(tokenStr string) (jwt.Claims, error) {
 		return nil, err
 	}
 	if !token.Valid {
-
+		return nil, errors.New("token is invalid")
 	}
 	exp, err := token.Claims.GetExpirationTime()
 	if err != nil {
@@ -39,7 +40,7 @@ func ValidJwtToken(tokenStr string) (jwt.Claims, error) {
 	}
 	isExpired := time.Now().UnixMilli() > exp.UnixMilli()
 	if isExpired {
-		return nil, err
+		return nil, errors.New("token is expired")
 	}
 	return token.Claims, nil
 }
